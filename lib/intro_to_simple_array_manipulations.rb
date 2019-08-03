4643cb0a5cb5bdd28fe3ff81b7697982fcc59e86
Taylor Watson <taylorwatsonb@gmail.com>
2019-08-03 16:27:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,8 +42,8 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, integer)
-   array.delete
+def using_delete_at(array, num)
+   array.delete_at(num)
 end
   
   
